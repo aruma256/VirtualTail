7a4618eb72c2f70665957b415f5fe2608035cded
aruma256 <22253623+aruma256@users.noreply.github.com>
2023-04-22 10:06:44 +0900

    監視対象外のアドレスへのOSCメッセージは無視する

--- tests/test_osc_value_provider.py
+++ tests/test_osc_value_provider.py
@@ -11,14 +11,24 @@ def test_init():
 
 
 def test_start():
-    # default value
     osc_listener = OSCValueProvider(port=PORT)
     osc_listener.start()
+    client = SimpleUDPClient("127.0.0.1", PORT)
+    # default value
     assert osc_listener.left is None
     assert osc_listener.right is None
     # update L value
-    client = SimpleUDPClient("127.0.0.1", PORT)
-    client.send_message("/avatar/parameters/Contact/Tail/L", 1)
+    client.send_message("/avatar/parameters/Contact/Tail/L", 0.25)
     time.sleep(0.1)
-    assert osc_listener.left == 1
+    assert osc_listener.left == 0.25
     assert osc_listener.right is None
+    # update R value
+    client.send_message("/avatar/parameters/Contact/Tail/R", 0.5)
+    time.sleep(0.1)
+    assert osc_listener.left == 0.25
+    assert osc_listener.right == 0.5
+    # ignore unsupported value
+    client.send_message("/avatar/parameters/Contact/Tail/U", 0.75)
+    time.sleep(0.1)
+    assert osc_listener.left == 0.25
+    assert osc_listener.right == 0.5
--- virtualtail/osc_value_provider.py
+++ virtualtail/osc_value_provider.py
@@ -5,25 +5,20 @@ from pythonosc.osc_server import BlockingOSCUDPServer
 
 
 class OSCValueProvider:
-    def __init__(
-            self,
-            ip="127.0.0.1",
-            port=9001) -> None:
+    def __init__(self, ip="127.0.0.1", port=9001) -> None:
         self._ip = ip
         self._port = port
         self._thread = None
         self.left = None
         self.right = None
 
-    def _osc_handler(self, address: str, value: float):
+    def _osc_handler(self, address: str, value: float) -> None:
         if address.endswith("L"):
             self.left = value
         elif address.endswith("R"):
             self.right = value
-        else:
-            raise RuntimeError()
 
-    def start(self):
+    def start(self) -> None:
         dispatcher = Dispatcher()
         dispatcher.map(
             "/avatar/parameters/Contact/Tail/*",
